0ba0e66c8372c4150c9d109db2642f9258a6a9e9
ender503 <ender503.tw@gmail.com>
2018-08-17 08:14:23 +0800

    Setup Redux Logger
    Use the Redux Logger to monitor the Redux state changed.

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import logger from 'redux-logger'
 
 const rootReducer = function (state = {}, action) {
   switch (action.type) {
@@ -19,7 +20,7 @@ const initialState = {}
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export default () => {
-  let store = createStore(persistedReducer, initialState)
+  let store = createStore(persistedReducer, initialState, applyMiddleware(logger))
   let persistor = persistStore(store)
   return { store, persistor }
 }
